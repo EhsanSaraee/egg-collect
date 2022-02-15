fe1e4fbb0a808448954cf4f64677c17ae60e1f9c
ehsan <ehsansaraee6570@yahoo.com>
2022-02-15 17:13:36 +0330

    content component development

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,9 +1,27 @@
+import ImageOne from '../Images/egg.jpg';
+import ImageTwo from '../Images/egg-2.jpg';
+
 const Content = () => {
-  return ( 
-    <>
-      <h1>Content</h1>
-    </>
+   return (
+      <>
+         <section className="menu-card">
+            <img src={ImageOne} alt="egg" className="image-card" />
+            <div className="center-content">
+               <h2 className="text-2xl mb-2">Egg Muffins</h2>
+               <p className="mb-2">Cripsy, delicious, and nutritious</p>
+               <span>$16</span>
+            </div>
+         </section>
+         <section className="menu-card">
+            <img src={ImageTwo} alt="egg" className="image-card" />
+            <div className="center-content">
+               <h2 className="text-2xl mb-2">Egg Muffins</h2>
+               <p className="mb-2">Cripsy, delicious, and nutritious</p>
+               <span>$18</span>
+            </div>
+         </section>
+      </>
    );
-}
- 
+};
+
 export default Content;
