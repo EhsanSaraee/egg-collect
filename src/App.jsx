870eb3d40bdfe88f02b7b421a5a7ccafb16f39cc
ehsan <ehsansaraee6570@yahoo.com>
2022-02-15 18:50:26 +0330

    dropdown menu logic added & finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Dropdown from './Components/Dropdown';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Home from './Pages';
@@ -6,9 +8,29 @@ import About from './Pages/About';
 import Menu from './Pages/Menu';
 
 const App = () => {
+   const [isOpen, setIsOpen] = useState(false);
+
+   const toggle = () => {
+      setIsOpen(!isOpen);
+   };
+
+   useEffect(() => {
+      const hideMenu = () => {
+         if (window.innerWidth > 768 && isOpen) {
+            setIsOpen(false);
+         }
+      };
+      window.addEventListener('resize', hideMenu);
+
+      return () => {
+         window.removeEventListener('resize', hideMenu);
+      };
+   }, [isOpen]);
+
    return (
       <>
-         <Navbar />
+         <Navbar toggle={toggle} />
+         <Dropdown toggle={toggle} isOpen={isOpen} />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
